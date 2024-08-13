c2c9a1387e2c0e3f23b690021ed4c7999c789549
FirassHossreh <firass.hus@gmail.com>
2024-08-13 23:45:23 +0300

    veri tipi duzeltme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:5000/todo/api')
-      .then(response => {
+    fetch("http://localhost:5000/todo/api")
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
-      .then(data => {
-        setTodos(data);
+      .then((data) => {
+        setTodos([data.message]);
         setLoading(false);
       })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
         setLoading(false);
       });
   }, []);
@@ -31,7 +31,7 @@ function App() {
       <h1>Todo List</h1>
       <ul>
         {todos.map((todo, index) => (
-          <li key={index}>{todo.title}</li>
+          <li key={index}>{todo}</li> // Verinin doğrudan gösterimi
         ))}
       </ul>
     </div>
