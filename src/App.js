4a6612e81c33780dd8e042d7656b7590d25c2a54
KusayAlkaleh <theonexr4@gmail.com>
2024-08-13 23:01:14 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/todo/api')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setTodos(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
