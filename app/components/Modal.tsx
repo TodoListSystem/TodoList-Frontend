031404634aa0358ab383466978fda63bd2f76dc5
KusayAlkaleh <theonexr4@gmail.com>
2024-08-21 05:28:41 +0300

    added Modal component to shared componets

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -9,7 +9,6 @@ const Modal: React.FC<ModalProps> = ({ isModalOpen, hideModal, children }) => {
     <dialog className={`modal ${isModalOpen ? "modal-open" : ""}`}>
       <div className="modal-box">
         <form method="dialog">
-          {/* if there is a button in form, it will close the modal */}
           <button
             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
             onClick={() => hideModal(false)}
