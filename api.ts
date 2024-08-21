dee2cee15047aa356902b48212cc32cce7f19938
KusayAlkaleh <theonexr4@gmail.com>
2024-08-21 05:26:36 +0300

    created "registerUser" function to handle registertion of new users

--- api.ts
+++ api.ts
@@ -1,23 +1,45 @@
-import { ITask } from "./types/tasks";
+import axios from "axios";
+import { FormObject, ITask } from "./types/tasks";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "http://localhost:5000/api/todolist/";
 
-export const getAllTasks = async (): Promise<ITask[]> => {
-  const resp = await fetch(`${BASE_URL}/tasks`);
-  const data = resp.json();
+// export const getAllTasks = async (): Promise<ITask[]> => {
+//   const resp = await fetch(`${BASE_URL}/tasks`);
+//   const data = resp.json();
 
-  return data;
-};
+//   return data;
+// };
 
-export const createNewTask = async (newTask: ITask): Promise<ITask> => {
-  const resp = await fetch(`${BASE_URL}/tasks`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newTask),
-  });
+// export const createNewTask = async (newTask: ITask): Promise<ITask> => {
+//   const resp = await fetch(`${BASE_URL}/tasks`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(newTask),
+//   });
 
-  const data: ITask = await resp.json();
-  return data;
+//   const data: ITask = await resp.json();
+//   return data;
+// };
+
+export const registerUser = (registerObj: FormObject): Promise<any> => {
+  return axios
+    .post(`${BASE_URL}/user/register`, registerObj)
+    .then((response) => {
+      // check the status code and save the token if the opertions has been success
+      if (response.status === 201) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+
+        return;
+      }
+
+      return Promise.reject(
+        new Error(`Unexpected status code: ${response.status}`)
+      );
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 };
