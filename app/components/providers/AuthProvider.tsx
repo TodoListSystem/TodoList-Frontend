1a79cf38a10dbef0873a821cf5ab8d816fc70ce1
KusayAlkaleh <theonexr4@gmail.com>
2024-08-21 05:29:55 +0300

    added init of auth for future

---
+++ app/components/providers/AuthProvider.tsx
@@ -0,0 +1,10 @@
+// "use client";
+
+// import { SessionProvider } from "next-auth/react";
+
+// function AuthProvider({ session, children }) {
+//   return;
+//   <SessionProvider session={session}>{children}</SessionProvider>;
+// }
+
+// export default AuthProvider;
