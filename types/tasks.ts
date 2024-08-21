3248b42010f32bde3339c48a190c9e61a29da5b9
KusayAlkaleh <theonexr4@gmail.com>
2024-08-21 05:31:09 +0300

    created new interface for handling type of form

--- types/tasks.ts
+++ types/tasks.ts
@@ -2,3 +2,11 @@ export interface ITask {
   id: string;
   text: string;
 }
+
+export interface FormObject {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
