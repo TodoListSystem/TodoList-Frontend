f46194782ed85e17aec88dac020058153d2209e1
KusayAlkaleh <theonexr4@gmail.com>
2024-08-25 16:35:55 +0300

    added IUser and ILogin types

--- types/globalTypes.ts
+++ types/globalTypes.ts
@@ -3,10 +3,16 @@ export interface ITask {
   text: string;
 }
 
-export interface FormObject {
+export interface IUser {
   userName: string;
   firstName: string;
   lastName: string;
+  email: string;
+  password: string;
+  _id?: string;
+}
+
+export interface ILogin {
   email: string;
   password: string;
 }
