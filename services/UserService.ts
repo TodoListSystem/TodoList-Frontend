ea4080aff25617d83a3eaa6c63ad24d769a5d32b
KusayAlkaleh <theonexr4@gmail.com>
2024-08-25 16:35:21 +0300

    updated user service

---
+++ services/UserService.ts
@@ -0,0 +1,53 @@
+import { IUser } from "@/types/globalTypes";
+import { ApiService } from "./ApiService";
+
+class UserService extends ApiService {
+  async registerUser(data: IUser): Promise<any> {
+    try {
+      const response = await this.post<any>("/user/register", data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser(data: any): Promise<any> {
+    try {
+      const response = await this.post<{ data: any }>("/user/login", data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  getUser(): IUser | null {
+    const dataInLocal = localStorage.getItem("user");
+
+    // handle the case where the data might be null
+    if (!dataInLocal) {
+      throw new Error("No user data found in local storage.");
+    }
+
+    try {
+      const userData: IUser = JSON.parse(dataInLocal).user;
+      return userData;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  isUserLoggedIn(): boolean {
+    const user = localStorage.getItem("user");
+    return user !== null;
+  }
+
+  async getUserProfile(userId: number): Promise<IUser> {
+    try {
+      return await this.get<IUser>(`/user/${userId}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export const userService = new UserService();
