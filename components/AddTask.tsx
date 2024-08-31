a5672edfc9e4dbfae7969ae9e487e90886a8d701
KusayAlkaleh <theonexr4@gmail.com>
2024-08-31 22:11:32 +0300

    updated AddTask componenet and re-style it & added error handeling

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -1,27 +1,60 @@
 "use client";
 
-import { FaPlus } from "react-icons/fa";
-import Modal from "./shared/Modal";
-import { FormEventHandler, useState } from "react";
-import { ITask } from "@/types/globalTypes";
+import Modal from "./ui/Modal";
+import toast from "react-hot-toast";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import { todoService } from "@/services/TodoService";
+import { INewTask, ITaskList } from "@/types/global-types";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 interface AddTaskProp {
-  handleCreatedNewTask: (newTask: ITask) => void;
+  handleCreatedNewTask: (newTask: ITaskList) => void;
 }
 
 const AddTask: React.FC<AddTaskProp> = ({ handleCreatedNewTask }) => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [addNewTask, setAddeNewTask] = useState<string>("");
+  const [itHasNote, setItHasNote] = useState<boolean>(false);
+  const [formObj, setFormObj] = useState<INewTask>({
+    title: "",
+    content: "",
+    note: "",
+  });
 
   const handleSubmitTask: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    // const resp = await createNewTask({
-    //   id: Math.floor(Math.random() * 1000).toString(),
-    //   text: addNewTask,
-    // });
-    // handleCreatedNewTask(resp);
-    // setAddeNewTask("");
-    // setModalOpen(false);
+
+    if (!formObj.title || !formObj.content) {
+      toast.error("the tile and content must not be empty!");
+      return;
+    }
+
+    try {
+      const response = await todoService.createTodo(formObj);
+      handleCreatedNewTask(response);
+      toast.success("Your task successfully added");
+
+      // reset states
+      setModalOpen(false);
+      setFormObj({
+        title: "",
+        content: "",
+        note: "",
+      });
+    } catch (error) {
+      toast.error("Your task has not been added");
+    }
+  };
+
+  const handleFormChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    const target = e.target as HTMLInputElement;
+    const { name, value } = target;
+
+    setFormObj((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
 
   return (
@@ -35,15 +68,61 @@ const AddTask: React.FC<AddTaskProp> = ({ handleCreatedNewTask }) => {
       </button>
       <Modal isModalOpen={modalOpen} hideModal={setModalOpen}>
         <form onSubmit={handleSubmitTask}>
-          <h3 className="text-lg font-bold">Add new task</h3>
-          <div className="modal-action">
+          <label className="form-control w-full mb-4">
+            <div className="label">
+              <h1 className="text-2xl">Title</h1>
+            </div>
             <input
               type="text"
-              value={addNewTask}
-              onChange={(e) => setAddeNewTask(e.target.value)}
-              placeholder="Type here your task..."
+              name="title"
+              value={formObj.title}
+              onChange={handleFormChange}
               className="input input-bordered w-full"
             />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <h1 className="text-2xl">Content</h1>
+            </div>
+            <textarea
+              name="content"
+              value={formObj.content}
+              onChange={handleFormChange}
+              className="textarea textarea-bordered textarea-lg w-full mb-4"
+            ></textarea>
+          </label>
+          <label className="form-control w-full cursor-pointer">
+            <div
+              className="label"
+              onClick={() => setItHasNote((prev) => !prev)}
+            >
+              <h1 className="text-lg flex items-center">
+                {itHasNote ? (
+                  <FaMinus className="mr-2" />
+                ) : (
+                  <FaPlus className="mr-2" />
+                )}
+                Add note for this todo
+              </h1>
+            </div>
+            {itHasNote && (
+              <input
+                type="text"
+                name="note"
+                value={formObj.note}
+                onChange={handleFormChange}
+                className="input input-bordered w-full"
+              />
+            )}
+          </label>
+          <div className="modal-action">
+            <button
+              type="button"
+              className="btn btn-neutral text-white"
+              onClick={() => setModalOpen(false)}
+            >
+              Close
+            </button>
             <button type="submit" className="btn btn-success text-white">
               Add Task
             </button>
